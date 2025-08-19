29e315883b1464d53b7d0cb1fe6ebdbf494caf77
Greg Conrad <gconrad0922@gmail.com>
2025-08-19 10:52:09 -0400

    Refactor plugin entry file

--- glacial-cpt-acf.php
+++ glacial-cpt-acf.php
@@ -12,97 +12,93 @@
  * @package         Glacial_Cpt_Acf
  **/
 
-/*
- * If this file is called directly, DIE!
- * */
-if ( !defined( 'ABSPATH' ) ) {
+if ( ! defined( 'ABSPATH' ) ) {
 	die;
 }
 
+// --- Constants ---
 define( 'GLACIAL_CPT_VERSION', '2.0.3' );
 define( 'GLACIAL_CPT_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'GLACIAL_CPT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'GLACIAL_CPT_TEMPLATES_FOLDER_NAME', 'cpt-acf-templates' );
-// get theme version
-$theme         = wp_get_theme();
-$theme_type_ok = $theme->get( 'Author' ) == 'Glacial Multimedia';
-$theme_ver_ok  = version_compare( $theme->get( 'Version' ), '3.0.0', '>=' );
 
-if ( get_template_directory() !== get_stylesheet_directory() ) {
-	$theme_ver_ok = version_compare( $theme->parent()->get( 'Version' ), '3.0.0', '>=' );
-}
-$has_acf = function_exists( 'the_field' );
+// --- Versioning ---
+define( 'CPT_STYLE_VERSION', get_option( 'blog_public' ) ? GLACIAL_CPT_VERSION : time() );
 
-/*
- * If "Discourage search engines from indexing this site" is checked in Settings > Reading
- * then add time() to static resources to prevent browser caching
- * */
-if ( get_option( 'blog_public' ) ) {
-	define( 'CPT_STYLE_VERSION', GLACIAL_CPT_VERSION );
-} else {
-	define( 'CPT_STYLE_VERSION', time() );
+// --- Theme/ACF Checks ---
+/**
+ * Checks if the active theme is Glacial and version >= 3.0.0.
+ *
+ * @return bool
+ */
+function glacial_theme_is_valid(): bool {
+	$theme = wp_get_theme();
+	$is_glacial = $theme->get( 'Author' ) === 'Glacial Multimedia';
+	$version_ok = version_compare( $theme->get( 'Version' ), '3.0.0', '>=' );
+
+	if ( get_template_directory() !== get_stylesheet_directory() ) {
+		$version_ok = version_compare( $theme->parent()->get( 'Version' ), '3.0.0', '>=' );
+	}
+	return $is_glacial && $version_ok;
 }
 
 /**
- * Plugin activation hook
+ * Checks if ACF is active.
  *
- * Add a flag that will allow to flush the rewrite rules when needed.
- *
- * @since 1.0.0
- * */
-function glacial_cpt_plugin_activate() {
-	//
-	if ( !get_option( 'glacial_flush_rewrite_rules_flag' ) ) {
+ * @return bool
+ */
+function glacial_acf_is_active(): bool {
+	return function_exists( 'the_field' );
+}
+
+// --- Activation/Deactivation ---
+/**
+ * Plugin activation hook.
+ */
+function glacial_cpt_plugin_activate(): void {
+	if ( ! get_option( 'glacial_flush_rewrite_rules_flag' ) ) {
 		add_option( 'glacial_flush_rewrite_rules_flag', true );
 	}
 }
-
 register_activation_hook( __FILE__, 'glacial_cpt_plugin_activate' );
 
-/**
- * Plugin deactivate hook
- *
- * Currently not in use
- *
- * @since 1.0.0
- * */
-function glacial_cpt_plugin_deactivate() {
-}
-
+function glacial_cpt_plugin_deactivate(): void {}
 register_deactivation_hook( __FILE__, 'glacial_cpt_plugin_deactivate' );
 
+// --- Admin Notices ---
 /**
- * if ACF is not installed or Glacial Theme not >= v3.0.0, show notice and don't load anything else
- *
- * @since 1.0.0
- * */
-if ( $has_acf && $theme_ver_ok && $theme_type_ok ) {
-	/**
-	 * Grab the main functions of our plugin
-	 *
-	 * @since 1.0.0
-	 * */
+ * Displays admin notices for missing requirements.
+ */
+function glacial_cpt_admin_notices(): void {
+	$acf = glacial_acf_is_active();
+	$theme_valid = glacial_theme_is_valid();
+	$theme = wp_get_theme();
+	$is_glacial = $theme->get( 'Author' ) === 'Glacial Multimedia';
+
+	?>
+    <div class="notice notice-error">
+        <h2>Glacial CPT Plugin</h2>
+		<?php if ( ! $acf ) : ?>
+            <h3>ACF Not Activated</h3>
+            <p>Please install and activate Advanced Custom Fields Pro, required by <b>Glacial Custom Post Types with ACF</b>.</p>
+		<?php endif; ?>
+		<?php if ( ! $is_glacial ) : ?>
+            <h3>Glacial Theme Not Activated</h3>
+            <p>Please install Glacial Theme, required by <b>Glacial Custom Post Types with ACF</b>.</p>
+		<?php endif; ?>
+		<?php if ( ! $theme_valid ) : ?>
+            <h3>Incorrect Version of Glacial Theme</h3>
+            <p>Please update to Glacial Theme v3.0.0+, required by <b>Glacial Custom Post Types with ACF</b>.</p>
+		<?php endif; ?>
+    </div>
+	<?php
+}
+
+// --- Main Loader ---
+if ( glacial_acf_is_active() && glacial_theme_is_valid() ) {
 	require GLACIAL_CPT_PLUGIN_DIR . 'includes/glacial-cpt-acf-main.php';
 	require GLACIAL_CPT_PLUGIN_DIR . 'shortcodes/locations.php';
 	require GLACIAL_CPT_PLUGIN_DIR . 'includes/glacial-cpt-acf-phone-modal.php';
-
 } else {
-	add_action( 'admin_notices', function () use ( $has_acf, $theme_ver_ok, $theme_type_ok ) { ?>
-        <div class="notice notice-error">
-            <h2>Glacial CPT Plugin</h2>
-			<?php if ( !$has_acf ) {
-				echo '<h3>ACF Not Activated</h3>';
-				echo '<p>Please install and activate Advanced Custom Fields Pro, it is required by <b>Glacial Custom Post Types with ACF</b> plugin to work.</p>';
-			}
-			if ( !$theme_type_ok ) {
-				echo '<h3>Glacial Theme Not Activated</h3>';
-				echo '<p>Please install Glacial Theme, it is required by <b>Glacial Custom Post Types with ACF</b> plugin to work.</p>';
-			}
-			if ( !$theme_ver_ok ) {
-				echo '<h3>Incorrect Version of Glacial Theme</h3>';
-				echo '<p>Please update to Glacial Theme v3.0.0+, it is required by <b>Glacial Custom Post Types with ACF</b> plugin to work.</p>';
-			} ?>
-        </div>
-		<?php
-	} );
+	add_action( 'admin_notices', 'glacial_cpt_admin_notices' );
 }
