6ef08cd1bd5e816ee9b49bf9e15028235acc0a28
Greg Conrad <gconrad0922@gmail.com>
2025-09-04 20:49:39 -0400

    Add conditional loading for phone modal and button
    Actions to add the phone modal and header button are now only registered if glacial_cpt_is_phone_modal_enabled() returns true. This prevents unnecessary output when the phone modal feature is disabled.

--- includes/glacial-cpt-acf-phone-modal.php
+++ includes/glacial-cpt-acf-phone-modal.php
@@ -16,8 +16,8 @@
  * This button is displayed in the header after the navigation menu.
  * It uses the `glacial_after_nav` action hook to insert the button.
  *
- * @since 2.1.0
  * @return void
+ * @since 2.1.0
  */
 function glacial_cpt_add_button_to_header(): void {
 
@@ -29,7 +29,6 @@ function glacial_cpt_add_button_to_header(): void {
 
 }
 
-add_action( 'glacial_after_nav', 'glacial_cpt_add_button_to_header', 1 );
 
 /**
  * @return void
@@ -39,5 +38,8 @@ function glacial_cpt_add_modal_to_footer(): void {
 	glacial_cpt_get_template_part( 'phone-modal' );
 }
 
-add_action( 'wp_footer', 'glacial_cpt_add_modal_to_footer', 100 );
+if ( glacial_cpt_is_phone_modal_enabled() ) {
+	add_action( 'wp_footer', 'glacial_cpt_add_modal_to_footer', 100 );
+	add_action( 'glacial_after_nav', 'glacial_cpt_add_button_to_header', 1 );
+}
 
