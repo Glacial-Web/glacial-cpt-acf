035b910e085a5dfbaa4ba66513ea9f5c65e8e05f
Greg Conrad <gconrad0922@gmail.com>
2025-09-08 21:24:38 -0400

    Version bump before release

--- glacial-cpt-acf.php
+++ glacial-cpt-acf.php
@@ -17,7 +17,7 @@ if ( !defined( 'ABSPATH' ) ) {
 }
 
 // --- Constants ---
-define( 'GLACIAL_CPT_VERSION', '2.0.3' );
+define( 'GLACIAL_CPT_VERSION', '2.1.0' );
 define( 'GLACIAL_CPT_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'GLACIAL_CPT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'GLACIAL_CPT_TEMPLATES_FOLDER_NAME', 'cpt-acf-templates' );
